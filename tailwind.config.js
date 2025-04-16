005192dabadb704419010358f5f3ca2f81aaedbd
unknown <mauijaloskor@gmail.com>
2025-04-16 12:27:29 +0600

    Folder structure done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1E40AF",
+        secondary: "#FBBF24",
+        accent: "#3B82F6",
+        light: {
+          100: "#F3F4F6",
+          200: "#E5E7EB",
+          300: "#D1D5DB",
+          400: "#9CA3AF",
+          500: "#6B7280",
+        },
+        dark: {
+          100: "#111827",
+          200: "#1F2937",
+          300: "#374151",
+          400: "#4B5563",
+          500: "#6B7280",
+        },
+      },
+    },
   },
   plugins: [],
 };
